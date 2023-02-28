8741611907d3c8f7fdfe7bfcea5915e608a31763
alikeli <eli-com2@yandex.ru>
2023-02-28 20:34:38 +0100

    authentication

--- src/main/java/com/lisa/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/lisa/springsecurity/config/SecurityConfig.java
@@ -1,23 +1,31 @@
 package com.lisa.springsecurity.config;
 
 import com.lisa.springsecurity.security.AuthProviderImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.lisa.springsecurity.service.PersonDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
-    @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+
+    public SecurityConfig(AuthProviderImpl authProvider, PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+
     }
 
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder.userDetailsService(personDetailsService);
+    }
 
-        authenticationManagerBuilder.authenticationProvider(authProvider);
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }

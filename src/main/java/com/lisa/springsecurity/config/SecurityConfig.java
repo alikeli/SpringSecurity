f70e121ce306b2baf75aa120ce622dfeee415d3a
alikeli <eli-com2@yandex.ru>
2023-03-01 18:10:19 +0100

    Custom auth.page

--- src/main/java/com/lisa/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/lisa/springsecurity/config/SecurityConfig.java
@@ -25,7 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable(). //отключили защиту
+                authorizeRequests()
                 .antMatchers("/auth/login", "/error").permitAll()
                 .anyRequest().authenticated()
                 .and()
